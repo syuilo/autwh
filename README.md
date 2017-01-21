245b200b2a091199e81bd743d1c2e329927cb03c
syuilo <syuilotan@yahoo.co.jp>
2017-01-21 12:44:32 +0900

    Initial commit :coffee:

---
+++ README.md
@@ -0,0 +1,53 @@
+autwh
+-------------------------------
+
+[![][npm-badge]][npm-link]
+[![][mit-badge]][mit]
+
+A simple Twitter API authentication helper. forked from [flutter](https://github.com/gosquared/flutter)
+
+## Install
+``` shell
+$ npm install autwh
+```
+
+## Usage
+### With [Express](https://github.com/expressjs/express)
+
+In this example, express-session is used, but **other methods are also possible.**
+
+``` javascript
+import autwh from 'autwh';
+import * as express from 'express';
+import * as session from 'express-session';
+
+const twAuth = autwh({
+	consumerKey: 'kyoppie',
+	consumerSecret: 'yuppie',
+	callbackUrl: 'https://my.app.example.com/twitter/callback'
+});
+
+const app = express();
+app.use(session());
+
+app.get('/twitter/connect', async (req, res) => {
+	const ctx = await twAuth.begin();
+	req.session.xtx = ctx;
+	res.redirect(ctx.url);
+});
+
+// URL used in loginCallback above
+app.get('/twitter/callback', async (req, res) => {
+	const tokens = await twAuth.done(req.session.ctx, req.query.oauth_verifier);
+	console.log(tokens);
+	res.send('Authorized!');
+});
+```
+
+## License
+[MIT](LICENSE)
+
+[npm-link]:  https://www.npmjs.com/package/autwh
+[npm-badge]: https://img.shields.io/npm/v/autwh.svg?style=flat-square
+[mit]:       http://opensource.org/licenses/MIT
+[mit-badge]: https://img.shields.io/badge/license-MIT-444444.svg?style=flat-square
---
+++ src/index.ts
@@ -0,0 +1,96 @@
+const OAuth = require('oauth').OAuth;
+
+export interface Options {
+	/**
+	 * Your app's consumer key
+	 */
+	consumerKey: string;
+
+	/**
+	 * Your app's consumer secret
+	 */
+	consumerSecret: string;
+
+	/**
+	 * The URL to redirect to after Twitter authorisation
+	 */
+	callbackUrl: string;
+}
+
+export type SessionContext = {
+	/**
+	 * Request token
+	 */
+	requestToken: string;
+
+	/**
+	 * Request token secret
+	 */
+	requestTokenSecret: string;
+}
+
+export type AccessTokens = {
+	/**
+	 * User's access token
+	 */
+	accessToken: string;
+
+	/**
+	 * User's access token secret
+	 */
+	accessTokenSecret: string;
+}
+
+export default function(opts: Options) {
+	const oauth = new OAuth(
+		'https://twitter.com/oauth/request_token',
+		'https://twitter.com/oauth/access_token',
+		opts.consumerKey,
+		opts.consumerSecret,
+		'1.0A',
+		opts.callbackUrl,
+		'HMAC-SHA1'
+	);
+
+	const begin = () => new Promise<SessionContext & {
+		/**
+		 * The URL that authenticate form
+		 */
+		url: string;
+	}>((resolve, reject) => {
+		oauth.getOAuthRequestToken((err, token, secret) => {
+			if (err) { // on error getting request tokens
+				return reject(err);
+			}
+
+			// got oauth request tokens
+			resolve({
+				requestToken: token,
+				requestTokenSecret: secret,
+
+				// sign in the user if the user has previously connected, else ask for authorization.
+				// see https://dev.twitter.com/oauth/reference/get/oauth/authenticate
+				url: `https://twitter.com/oauth/authenticate?oauth_token=${token}`
+			});
+		});
+	});
+
+	const done = (ctx: SessionContext, verifier: string) => new Promise<AccessTokens>((resolve, reject) => {
+		oauth.getOAuthAccessToken(ctx.requestToken, ctx.requestTokenSecret, verifier, (err, token, secret) => {
+			if (err) { // on error getting access tokens
+				return reject(err);
+			}
+
+			// got oauth access tokens
+			resolve({
+				accessToken: token,
+				accessTokenSecret: secret
+			});
+		});
+	});
+
+	return {
+		begin,
+		done
+	};
+}
